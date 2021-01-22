fad23f6868c35aa96b740d092473e107391fb44a
matsufuji06 <n.matsufuji06@gmail.com>
2021-01-22 10:04:36 +0900

    initial commit

--- memo_app/app/controllers/todos_controller.rb
+++ memo_app/app/controllers/todos_controller.rb
@@ -1,2 +1,16 @@
 class TodosController < ApplicationController
+  def index
+    @todo = Todo.new
+    @todos = Todo.order('created_at ASC')
+  end
+
+  def create
+    Todo.create(todo_params)
+    redirect_to :root
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:content)
+  end
 end

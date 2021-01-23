a15cb347b57d5ffcc590555f18682714bd8f9684
matsufuji06 <n.matsufuji06@gmail.com>
2021-01-23 09:11:27 +0900

    ajaxメソッドを記述

--- memo_app/app/assets/javascripts/todo.js
+++ memo_app/app/assets/javascripts/todo.js
@@ -2,6 +2,15 @@ $(function() {
   $('.js-form').on('submit', function(e) {
     e.preventDefault();
     var todo = $('.js-form__text-field').val();
-    console.log(todo);
+    $.ajax({
+      type: 'POST',
+      url: '/todos',
+      data: {
+        todo: {
+          content: todo
+        }
+      },
+      dataType: 'json'
+    })
   });
 });
